c7cc1b72adcd1798bbc51266eadfad9ca2a82cfd
Taher <ttaherhagui@gmail.com>
2022-03-21 23:01:36 +0100

    fix linter.

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,8 +3,6 @@ class Api::MessagesController < ApplicationController
     ids = Message.ids
     random_greeting_id = ids.sample
     random_greeting = Message.find(random_greeting_id).greeting
-    render json: { message: random_greeting}
+    render json: { message: random_greeting }
   end
-  private
-
 end
